b171083790936348fee82290eacf1e6a940ace48
FelipeMovio <felipelsmovio@gmail.com>
2026-08-03 16:20:23 -0300

    criando classe Musica

---
+++ ConsoleApp1/ConsoleApp1/Musica.cs
@@ -0,0 +1,23 @@
+﻿class Musica
+{
+    public String nome{ get; set; }
+    public String artista{ get; set; }
+    public int duracao{ get; set; }
+    public bool disponivel { get; set; }
+
+
+    public void exibirFichaTecnica()
+    {
+        Console.WriteLine("Nome: " + nome);
+        Console.WriteLine("Artista: " + artista);
+        Console.WriteLine("Duração: " + duracao + " segundos");
+        if (disponivel){
+            Console.WriteLine("Disponível: Sim");
+        }
+        else {
+            Console.WriteLine("Disponível: Não");
+        }
+    }
+
+
+}
