6e206fa7a59a2c23f9f96b22ae46ff5c6a56eb89
FelipeMovio <felipelsmovio@gmail.com>
2026-08-03 23:52:06 -0300

    update

--- ConsoleApp1/ConsoleApp1/Album.cs
+++ ConsoleApp1/ConsoleApp1/Album.cs
@@ -1,1 +1,32 @@
-﻿
+﻿class Album
+{
+    public string Nome { get; set; }
+
+    public int DuracaoTotal =>
+        musicas.Sum(m => m.Duracao);
+
+    private List<Musica> musicas = new List<Musica>();
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+     
+    }
+
+    public void ExibirMusicas()
+    {
+        Console.WriteLine("Músicas do álbum " + Nome + ":");
+
+        //for(int i = 0; i < musicas.Count; i++)
+        //{
+        //    Console.WriteLine("- " + musicas[i].Nome);
+        //}
+
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine("- " + musica.Nome);
+        }
+
+        Console.WriteLine($"Para ouvir todas as músicas do álbum, você precisará de {DuracaoTotal} segundos.");
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,4 +4,22 @@ musica1.Artista = "Ed Sheeran";
 musica1.Duracao = 240;
 musica1.Disponivel = true;
 
-musica1.exibirFichaTecnica();
+Album albumDoQuen = new Album();
+albumDoQuen.Nome = "A nigth at the opera";
+
+Musica musica2 = new Musica();
+musica2.Nome = "Bohemian Rhapsody";
+musica2.Artista = "Queen";
+musica2.Duracao = 213;
+musica2.Disponivel = true;
+
+Musica musica3 = new Musica();
+musica3.Nome = "Love of my life";
+musica3.Artista = "Queen";
+musica3.Duracao = 198;
+musica3.Disponivel = true;
+
+albumDoQuen.AdicionarMusica(musica2);
+albumDoQuen.AdicionarMusica(musica3);
+
+albumDoQuen.ExibirMusicas();
