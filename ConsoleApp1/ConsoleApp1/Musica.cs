087e6a90d4551986c6e9b1ccf8e46f6d588f0792
FelipeMovio <felipelsmovio@gmail.com>
2026-08-04 00:02:24 -0300

    update/create class Genero

--- ConsoleApp1/ConsoleApp1/Musica.cs
+++ ConsoleApp1/ConsoleApp1/Musica.cs
@@ -5,12 +5,15 @@
     public int Duracao{ get; set; }
     public bool Disponivel { get; set; }
 
+    public Genero Genero { get; set; }
+
     public string DescricaoResumida =>
           $"A musica {Nome} do artista {Artista}";
 
 
     public void exibirFichaTecnica()
     {
+        Console.WriteLine($"Genero: {Genero.Nome}");
         Console.WriteLine("Nome: " + Nome);
         Console.WriteLine("Artista: " + Artista);
         Console.WriteLine("Duração: " + Duracao + " segundos");
